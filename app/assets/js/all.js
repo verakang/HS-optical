4e9ede446e501bc5e3b4a58dd2524fcb99b3e848
Vera <sk70885@hotmail.com>
2022-07-31 15:26:54 +0800

    Update 2022-07-31T07:26:48.869Z

--- app/assets/js/all.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-    $('.mav-x-mobile').on('click', function (e) {
-        e.preventDefault();
-        $('.nav-item').slideToggle('show-item');
-    });
-});
